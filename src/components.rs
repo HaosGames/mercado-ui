8f1f378af7d2caa8f5d61d7163ed4ba6a2c00a75
Haos Games <haosgames@protonmail.com>
2023-08-23 16:08:52 +0200

    add page for prediction overview

--- src/components.rs
+++ src/components.rs
@@ -1,15 +1,18 @@
 #![allow(non_snake_case)]
 use anyhow::Result;
 use leptos::*;
+use leptos_router::*;
 use mercado::api::*;
 use mercado::client::Client;
+use mercado::secp256k1::{generate_keypair, rand};
 
 pub fn App(cx: Scope) -> impl IntoView {
     let (count, set_count) = create_signal(cx, 10);
-    let predictions = create_local_resource(cx, move || count.get(), query_predictions);
+    let predictions = create_local_resource(cx, move || count.get(), get_predictions);
 
     view! { cx,
         <div>
+            <Outlet/>
         {
             move || match predictions.read(cx) {
                 None => view! {cx, <p>"Loading..."</p>}.into_view(cx),
@@ -41,19 +44,56 @@ pub fn App(cx: Scope) -> impl IntoView {
 pub fn PredictionListItem(cx: Scope, prediction: PredictionListItemResponse) -> impl IntoView {
     view! {cx,
         <li>
-            <p>{prediction.name}</p>
-            <p>" | True: "{prediction.bets_true}"sats, "
+            <a href={format!("prediction/{}", prediction.id)}>{prediction.name}</a>
+            <p>"True: "{prediction.bets_true}"sats, "
             "False: "{prediction.bets_false}"sats"
             " | Ends "{prediction.trading_end.to_rfc2822()}
             " | Judge share: "{prediction.judge_share_ppm / 1000}"%"</p>
         </li>
     }
 }
-async fn query_predictions(_how_many: u32) -> Result<Vec<PredictionListItemResponse>, String> {
+async fn get_predictions(_how_many: u32) -> Result<Vec<PredictionListItemResponse>, String> {
     let client = Client::new("http://127.0.0.1:8081".to_string());
-    let result = client.get_predictions().await;
-    match result {
-        Ok(predictions) => Ok(predictions),
-        Err(e) => Err(format!("{:#}", e)),
+    client.get_predictions().await.map_err(map_any_err)
+}
+async fn get_user_prediction(
+    prediction: RowId,
+    user: UserPubKey,
+) -> Result<UserPredictionOverviewResponse, String> {
+    let client = Client::new("http://127.0.0.1:8081".to_string());
+    client
+        .get_user_prediction(prediction, user)
+        .await
+        .map_err(map_any_err)
+}
+
+#[component]
+pub fn UserPredictionOverview(cx: Scope) -> impl IntoView {
+    let params = use_params_map(cx);
+    let prediction = create_local_resource(
+        cx,
+        move || params.with(|p| p.get("id").cloned().unwrap_or_default()),
+        move |id| {
+            get_user_prediction(
+                id.parse().unwrap_or_default(),
+                generate_keypair(&mut rand::thread_rng()).1,
+            )
+        },
+    );
+    view! {cx,
+        <div>
+        {
+            move || match prediction.read(cx) {
+                None => view! {cx, <p>"Loading..."</p>}.into_view(cx),
+                Some(Ok(prediction)) => view! {cx,
+                    <div>
+                        <h2>{prediction.name}</h2>
+                    </div>
+                }.into_view(cx),
+                Some(Err(e)) => view! {cx, <p>{format!("Got error: {:?}", e)}</p>}.into_view(cx),
+            }
+        }
+        </div>
+
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
 use crate::components::*;
 use leptos::*;
+use leptos_router::*;
 
 mod components;
 
 fn main() {
-    mount_to_body(|cx| view! { cx,  <App/> })
+    mount_to_body(|cx| {
+        view! { cx,
+            <div id="root">
+                <Router>
+                    <nav>
+                        <a href="/">"Home"</a>
+                    </nav>
+                    <main>
+                        <Routes>
+                            <Route path="" view=App/>
+                            <Route path="prediction" view=move |_| view! {cx, <Outlet/>}>
+                                <Route path=":id" view=UserPredictionOverview/>
+                                <Route path="" view=App/>
+                            </Route>
+                        </Routes>
+                    </main>
+                </Router>
+            </div>
+        }
+    })
 }
-
