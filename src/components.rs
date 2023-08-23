428c6d3418dcb35a79e52487db2caa6a10c927fd
Haos Games <haosgames@protonmail.com>
2023-08-23 12:20:29 +0200

    query for predictions

--- src/components.rs
+++ src/components.rs
@@ -1,5 +1,5 @@
 #![allow(non_snake_case)]
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use leptos::*;
 use mercado::api::*;
 use mercado::client::Client;
@@ -13,7 +13,22 @@ pub fn App(cx: Scope) -> impl IntoView {
         {
             move || match predictions.read(cx) {
                 None => view! {cx, <p>"Loading..."</p>}.into_view(cx),
-                Some(Ok(predictions)) => view! {cx, <p>{predictions.len()}</p>}.into_view(cx),
+                Some(Ok(predictions)) => view! {cx,
+                    <div>
+                        <p>{predictions.len()}" prediction(s)"</p>
+                        <ul>
+                            {predictions.into_iter()
+                                .map(|prediction| view! {cx,
+                                    <li>{prediction.name}
+                                    " | True: "{prediction.bets_true}"sats, "
+                                    "False: "{prediction.bets_false}"sats"
+                                    " | Ends "{prediction.trading_end.to_rfc2822()}
+                                    " | Judge share: "{prediction.judge_share_ppm / 1000}"%"</li>
+                                })
+                                .collect::<Vec<_>>()}
+                        </ul>
+                    </div>
+                }.into_view(cx),
                 Some(Err(e)) => view! {cx, <p>{format!("Got error: {:?}", e)}</p>}.into_view(cx),
             }
         }
